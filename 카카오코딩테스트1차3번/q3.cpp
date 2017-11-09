b96f4e6c3b6eb6cefd6b7d37817021b3c5357acf
mgh3326 <mgh3326@naver.com>
2017-11-09 12:08:03 +0900

    first

---
+++ 카카오코딩테스트1차3번/q3.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<stack>
+using namespace std;
+vector<string> v;
+void function(istream& ins)
+{
+	stack<string> st;
+	int i=0;
+	string str;
+	char c;
+	while (ins && ins.peek() != '\n')
+	{
+		cout << "test" << endl;
+		if(ins.peek()==',')
+		{
+			//ins >> c;
+			cin.ignore();
+		}
+		if(isalpha(ins.peek())){
+			cout << "test" << endl;
+			ins >> c;
+			str.push_back(c);
+			cout << c << endl;
+			cout << "test" << endl;
+			st.push(str);
+			cout << str << endl;
+		}
+		else {
+			ins.ignore();
+
+		}
+	}
+	for (int i = 0; i < st.size(); i++)
+	{
+		cout << st.top() << endl;
+		st.pop();
+	}
+	
+}
+int main(void){
+	//cout<<"test"<<endl;
+	int cashsize=0;
+	cin>>cashsize;
+	function(cin);
+	return 0;
+}
